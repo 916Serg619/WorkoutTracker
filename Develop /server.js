13d3f04035f8ca76f979025512527534713be512
Sergio Aguirre <sergioaguirre@Sergios-MBP.attlocal.net>
2021-02-09 15:42:40 -0800

    mongodb line17 server.js

--- Develop /server.js
+++ Develop /server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 //Establish connection to MongoDB//
-mongoose.connect(process.env.MONGDB_URI || "mongodb://localhost/dbWorkout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbWorkout", {
     useNewUrlParser: true,
     useFindAndModify: false,
     useUnifiedTopology: true
