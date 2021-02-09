983d23a3eae4db4737b7b69787124271037f798a
Sergio Aguirre <sergioaguirre@Sergios-MBP.attlocal.net>
2021-02-09 15:16:44 -0800

    changed syntax error server.js line 17

--- Develop /server.js
+++ Develop /server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 //Establish connection to MongoDB//
-mongoose.connect(process.env.MONOGOD_URI || "mongodb://localhost/dbWorkout", {
+mongoose.connect(process.env.MONOGODB_URI || "mongodb://localhost/dbWorkout", {
     useNewUrlParser: true,
     useFindAndModify: false,
     useUnifiedTopology: true
